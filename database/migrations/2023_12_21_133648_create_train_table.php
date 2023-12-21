cdd8293adc4d0a043a48378e6607d7938c8121e4
federicomanni97 <manni_federico@libero.it>
2023-12-21 15:04:54 +0100

    table schema completed

--- database/migrations/2023_12_21_133648_create_train_table.php
+++ database/migrations/2023_12_21_133648_create_train_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 255);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time')->unsigned();
+            $table->dateTime('arrival_time')->unsigned();
+            $table->string('train_code', 100);
+            $table->tinyInt('cab_number', 20)->unsigned();
+            $table->string('punctual_train');
+            $table->string('deleted_train');
             $table->timestamps();
         });
     }
